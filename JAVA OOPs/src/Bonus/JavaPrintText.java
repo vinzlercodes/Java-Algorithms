c57ec1f424e96ff31e79cdee165dc3782b16fd6c
Vinayak Sengupta <vinayak.sengupta@gmail.com>
2021-01-04 11:25:45 +0530

    Hello World and Bonus

---
+++ JAVA OOPs/src/Bonus/JavaPrintText.java
@@ -0,0 +1,20 @@
+package Bonus;
+
+public class JavaPrintText {
+    // main method begins execution of the Java Class
+
+       /* The following class prints out
+       a short excerpt from a book
+       by Lewis Carroll*/
+
+    public static void main( String[] args )
+    {
+        System.out.println( "\"Begin at the");
+        System.out.println( "begining,\", the King");
+        System.out.println( "said, very gravely, \"and");
+        System.out.println( "go on till you come to");
+        System.out.println( "the end: then stop.\"");
+        System.out.println( "           -Lewis Carroll");
+
+    } // end method main
+} // end class
---
+++ JAVA OOPs/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class HelloWorld
+{
+    // Program prints out "Hello, World!"
+    public static void main(String args[]){
+
+        System.out.println("Hello World!");
+    }
+}
